f5f9ba10f6e004ae579f2d443e62a90cdfcd8b0f
Jean Sacren <sakiwit@gmail.com>
2026-06-05 13:49:51 -0600

    Add custom server.hmr.host for vite
    Reset server.hmr.host so that WebSocket connections (for live updates)
    point to laravel.local instead of default localhost.

--- vite.config.js
+++ vite.config.js
@@ -24,6 +24,9 @@ export default defineConfig({
     ],
     server: {
         cors: true,
+        hmr: {
+            host: 'laravel.local',
+        },
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
